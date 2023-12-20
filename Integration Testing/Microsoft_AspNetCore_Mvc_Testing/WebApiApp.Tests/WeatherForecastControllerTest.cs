c8ed00480554c658b941a4f896a3eee83e14d8d0
Nancy Li <NancyLi@Eurofins.com>
2023-12-20 13:26:31 +0800

    Update

---
+++ Integration Testing/Microsoft_AspNetCore_Mvc_Testing/WebApiApp.Tests/WeatherForecastControllerTest.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebApiApp.Tests
+{
+    [TestClass]
+    public class WeatherForecastControllerTest
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public WeatherForecastControllerTest()
+        {
+            _factory = new WebApplicationFactory<Startup>();
+        }
+
+        [TestMethod]
+        public async Task WeatherForecastControllerTest_Get_WeatherForecast()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/WeatherForecast");
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.AreEqual("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
---
+++ Integration Testing/Microsoft_AspNetCore_Mvc_Testing/WebApiApp/Program.cs
@@ -0,0 +1,52 @@
+namespace WebApiApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
+    }
+
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+        }
+    }
+
+}
